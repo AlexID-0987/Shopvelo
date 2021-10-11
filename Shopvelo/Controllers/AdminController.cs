8858df60b518e2fa1410955ede507163abdbcba6
AlexID-0987 <a1980dom@gmail.com>
2021-10-11 23:36:26 +0300

    Страница Админа

---
+++ Shopvelo/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopvelo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopvelo.Controllers
+{
+    public class AdminController : Controller
+    {
+        Bakecontext context;
+
+        public AdminController(Bakecontext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(context.Bakes.ToList());
+        }
+    }
+}
